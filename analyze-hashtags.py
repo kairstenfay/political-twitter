f770e38a4441568feb043911a44eb11a6a5bdbff
Kairsten Fay <kairsten.fay@gmail.com>
2019-11-10 01:00:24 -0800

    wip wordmaps

--- analyze-hashtags.py
+++ analyze-hashtags.py
@@ -5,17 +5,15 @@ import json
 import collections
 import logging
 import argparse
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
 from typing import Any, List, Dict, Optional
 from itertools import groupby
 from collections import Counter
-# -- wordmap
-import multidict as multidict
-import numpy as np
-from PIL import Image
+# from PIL import Image
 from os import path
 from wordcloud import WordCloud, ImageColorGenerator
-import matplotlib.pyplot as plt
-import imageio
 
 
 LOG = logging.getLogger(__name__)
@@ -28,26 +26,25 @@ def build_hashtags_list(hashtags: List[Dict[str, Any]]) -> Optional[str]:
         return text.lower()
 
 
-def makeImage(text):
-    # alice_mask = np.array(Image.open("us-flag.png", pilmode='RGB'))
-    alice_mask = imageio.imread("us-flag.png", as_gray=False, pilmode="RGB")
+def make_image(text, mask_name: str, screen_name: str):
+    image_mask = imageio.imread("us-flag.png", as_gray=False, pilmode="RGB")
 
     # create coloring from image
-    image_colors = ImageColorGenerator(alice_mask)
+    image_colors = ImageColorGenerator(image_mask)
 
-    wc = WordCloud(background_color="white", max_words=1000, repeat=True, mask=alice_mask)
-    # generate word cloud
+    wc = WordCloud(background_color="white", max_words=1000, repeat=True, mask=image_mask)
     wc.generate_from_frequencies(text)
 
-    # show
     # axes[0].imshow(wc, interpolation="bilinear")
     # recolor wordcloud and show
     # we could also give color_func=image_colors directly in the constructor
+    # fig = plt.figure()
     plt.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
     # axes[2].imshow(alice_mask, cmap=plt.cm.gray, interpolation="bilinear")
     #for ax in axes:
     plt.axis("off")
-    plt.show()
+    plt.savefig(f'wordmaps/{screen_name}-{mask_name}')
+    # plt.show()
 
 
 if __name__ == '__main__':
@@ -78,7 +75,7 @@ if __name__ == '__main__':
             if not dict(counter):
                 continue
 
-            makeImage(dict(counter))
+            make_image(dict(counter), 'us-flag.png', screen_name)
 
             if screen_name in stats:
                 print(screen_name, stats[screen_name]['top_5_hashtags'])
