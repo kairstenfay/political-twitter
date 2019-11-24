11d64d8edb32c01c1f19ea8408cb4e4a6b0878f9
Kairsten Fay <kairsten.fay@gmail.com>
2019-11-24 00:34:24 -0800

    Add candidate photos

--- scripts/search_tweets_from_users.sh
+++ scripts/search_tweets_from_users.sh
@@ -1,8 +1,8 @@
 #! ./bin/bash
 #
 # 'AOC' 'IlhanMN' 'RashidaTlaib' 'AyannaPressley'
-for screen_name in 'BernieSanders' 'CoryBooker' # 'JoeBiden' 'realDonaldTrump' 'AndrewYang' 'TulsiGabbard' 'amyklobuchar' 'KamalaHarris' 'PeteButtigieg' 'ewarren'
+for screen_name in 'TomSteyer' # 'BernieSanders' 'CoryBooker' 'JoeBiden' 'realDonaldTrump' 'AndrewYang' 'TulsiGabbard' 'amyklobuchar' 'KamalaHarris' 'PeteButtigieg' 'ewarren'
     do
         echo 'searching tweets in the timeline of' $screen_name
-        python search-tweets.py -u $screen_name -d 2018-11-13 > data/$screen_name-tweets.ndjson
+        python3 search-tweets.py -u $screen_name -d 2018-11-13 > data/$screen_name-tweets.ndjson
     done
--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import styled from "styled-components"
+
+import Warren from "./img/ewarren.png"
+import Sanders from "./img/BernieSanders.png"
+import Biden from "./img/JoeBiden.png"
 import Yang from "./img/AndrewYang.png"
+import Buttigieg from "./img/PeteButtigieg.png"
+import Booker from "./img/CoryBooker.png"
+import Gabbard from "./img/TulsiGabbard.png"
+import Harris from "./img/KamalaHarris.png"
+import Klobuchar from "./img/AmyKlobuchar.png"
+// import Steyer from "./img/TomSteyer.png"
 
 const Img = styled.img`
   border-radius: 50%;
   max-width: 100px;
-  border: 1px solid black;
+  border: 1px solid grey;
+  margin: 5px;
 `
 
+const candidates = [
+    Warren, Sanders, Biden, Yang, Buttigieg, Gabbard, Harris, Klobuchar, Booker
+]
 
-const Candidate = () => (<Img src={Yang} />)
+const Candidate = (props) => (<Img src={props.candidate} />)
 
 const App = () => {
     return (
         <div>
-            <Candidate />
+            { candidates.map(c => (<Candidate candidate={c} />)) }
         </div>
     )
 }
 
-const domElement = document.getElementById("root")
 
-// and away we go!
+const domElement = document.getElementById("root")
 ReactDOM.render(<App />, domElement)
