4a9872a06073312412f58035ba5df0d1777378ff
Kairsten Fay <kairsten.fay@gmail.com>
2019-11-24 22:21:36 -0800

    wip
    Add status for click on/ click off.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import ReactDOM from "react-dom"
 import styled from "styled-components"
+import './main.css';
 
 import Warren from "./img/ewarren.png"
 import Sanders from "./img/BernieSanders.png"
@@ -15,55 +16,115 @@ import Klobuchar from "./img/AmyKlobuchar.png"
 
 const Img = styled.img`
   border-radius: 50%;
-  max-width: 100px;
-  border: 1px solid grey;
+  max-width: 15vw;
   margin: 5px;
 `
 
-const H1 = styled.h1``
+const ToolTip = styled.p`
+  background-color: lightgrey;
+  padding: 3vw;
+  max-width: 30vw;
+  border-radius: 5px;
+`
 
-const candidates = [
-    Warren, Sanders, Biden, Yang, Buttigieg, Gabbard, Harris, Klobuchar, Booker
-]
+const candidateMap = {
+    'Elizabeth Warren': { name: 'Elizabeth Warren', photo: Warren },
+    'Bernie Sanders': { name: 'Bernie Sanders', photo: Sanders },
+    'Joe Biden': { name: 'Joe Biden', photo: Biden },
+    'Andrew Yang': { name: 'Andrew Yang', photo: Yang },
+    'Pete Buttigieg': { name: 'Pete Buttigieg', photo: Buttigieg },
+    'Tulsi Gabbard': { name: 'Tulsi Gabbard', photo: Gabbard },
+    'Kamala Harris': { name: 'Kamala Harris', photo: Harris },
+    'Amy Klobuchar': { name: 'Amy Klobuchar', photo: Klobuchar },
+    'Cory Booker': { name: 'Cory Booker', photo: Booker },
+}
 
 const Candidate = (props) => (
-    <Img src={props.candidate}
+    <Img src={props.candidate.photo}
+        ref={props.node}
         onMouseEnter={props.handleHover}
         onMouseOut={props.handleHover}
-        />)
+        onClick={props.handleClick}
+        />
+    )
 
-
-const Tooltip = (props) => {
+const CandidatePreview = (props) => {
     return (
-        <div id="tooltip">
+        <ToolTip id="tooltip">
             { props.children }
-        </div>
+        </ToolTip>
     )
 }
 
 
-
 const App = () => {
     const [ toolTipVisibility, setToolTipVisibility ] = useState(false)
-    const toggleToolTipVisibility = (toolTipVisibility) => {  // useEffect?
+    const [ currentCandidate, setCurrentCandidate ] = useState(null)
+    const [ hoverTarget, setHoverTarget ] = useState(null)
+    const node = useRef();
+
+
+    const handleClick = e => {
+        if (node.current.contains(e.target)) {
+          // inside click
+          return;
+        }  // outside click
+        setCurrentCandidate(null)
+      };
+
+    useEffect(() => {
+        // add when mounted
+        document.addEventListener("mousedown", handleClick);
+
+        // return function to be called when unmounted
+        return () => {
+          document.removeEventListener("mousedown", handleClick);
+        };
+      }, []);
+
+    const toggleToolTipVisibility = (candidate) => {  // useEffect?
         setToolTipVisibility(!toolTipVisibility)
-    } // TODO
+        setHoverTarget(candidate)
+    }
+
+    const toggleCurrentCandidate = () => {
+    }
+
+    // useEffect(() => {
+    //     if (toolTipVisibility) {
+    //         setCurrentCandidate('testing')
+    //     }
+    // }, [ toolTipVisibility ]) // TODO
 
     return (
         <div>
             <h1>U.S. Democratic Presidential Candidates</h1>
             <h2>A Tweet analysis</h2>
-            { candidates.map((c, i) => (
+            { Object.values(candidateMap).map((c, i) => (
                 <Candidate
+                    node={node}
                     key={i}
                     candidate={c}
-                    handleHover={() => setToolTipVisibility(!toolTipVisibility)}
+                    handleHover={c => toggleToolTipVisibility(c.name)}
+                    handleClick={() => setCurrentCandidate(c.name)}
                     />
             )) }
-            { toolTipVisibility && (
-                <Tooltip>
-                    Hello, candidate!
-                </Tooltip>
+            <p>image credit: Politico</p>
+            { toolTipVisibility? (
+                <CandidatePreview>
+                    Click to see stats on {hoverTarget}
+                </CandidatePreview>
+            ) : (
+                <CandidatePreview>
+                    Click on a candidate to see their stats.
+                </CandidatePreview>
+            )}
+            { !!currentCandidate && (
+                <ToolTip>
+                    Some facts about {currentCandidate}:
+                    * loves cheese
+                    * has a Twitter account
+                </ToolTip>
             )}
         </div>
     )
