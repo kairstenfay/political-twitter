8135ed88473b9505f7ea081c47a8260a4fc5ffc6
Kairsten Fay <kairsten.fay@gmail.com>
2019-11-25 20:49:19 -0800

    Add proper hover/click functionality on photos

--- src/index.js
+++ src/index.js
@@ -14,18 +14,6 @@ import Harris from "./img/KamalaHarris.png"
 import Klobuchar from "./img/AmyKlobuchar.png"
 // import Steyer from "./img/TomSteyer.png"
 
-const Img = styled.img`
-  border-radius: 50%;
-  max-width: 15vw;
-  margin: 5px;
-`
-
-const ToolTip = styled.p`
-  background-color: lightgrey;
-  padding: 3vw;
-  max-width: 30vw;
-  border-radius: 5px;
-`
 
 const candidateMap = {
     'Elizabeth Warren': { name: 'Elizabeth Warren', photo: Warren },
@@ -39,8 +27,22 @@ const candidateMap = {
     'Cory Booker': { name: 'Cory Booker', photo: Booker },
 }
 
+const Img = styled.img`
+  border-radius: 50%;
+  max-width: 15vw;
+  margin: 5px;
+`
+
+const ToolTip = styled.div`
+  background-color: lightgrey;
+  padding: 3vw;
+  max-width: 30vw;
+  border-radius: 5px;
+`
+
 const Candidate = (props) => (
     <Img src={props.candidate.photo}
+        id={props.name}
         ref={props.node}
         onMouseEnter={props.handleHover}
         onMouseOut={props.handleHover}
@@ -48,53 +50,50 @@ const Candidate = (props) => (
         />
     )
 
-const CandidatePreview = (props) => {
+const CandidateStats = (props) => {
     return (
         <ToolTip id="tooltip">
+            Some facts about {props.candidate}:
+            <ul>
+                <li>loves cheese</li>
+                <li>has a Twitter account</li>
+            </ul>
             { props.children }
         </ToolTip>
     )
 }
 
-
 const App = () => {
-    const [ toolTipVisibility, setToolTipVisibility ] = useState(false)
-    const [ currentCandidate, setCurrentCandidate ] = useState(null)
-    const [ hoverTarget, setHoverTarget ] = useState(null)
+    const [ clickedCandidate, setClickedCandidate ] = useState(null)
+    const [ hoverCandidate, setHoverCandidate ] = useState(null)
     const node = useRef();
 
-
+    /**
+     * Reset the clickedCandidate to `null` on click-away events.
+     * @param {*} e
+     */
     const handleClick = e => {
         if (node.current.contains(e.target)) {
-          // inside click
           return;
         }  // outside click
-        setCurrentCandidate(null)
+        setClickedCandidate(null)
       };
 
+    const handleHover = (candidate) => {  // useEffect?
+        hoverCandidate ? setHoverCandidate(null) : setHoverCandidate(candidate)
+    }
+
+    /**
+     * Handle click and click-away effects.
+     */
     useEffect(() => {
         // add when mounted
         document.addEventListener("mousedown", handleClick);
 
-        // return function to be called when unmounted
-        return () => {
+        return () => { // return function to be called when unmounted
           document.removeEventListener("mousedown", handleClick);
         };
       }, []);
-
-    const toggleToolTipVisibility = (candidate) => {  // useEffect?
-        setToolTipVisibility(!toolTipVisibility)
-        setHoverTarget(candidate)
-    }
-
-    const toggleCurrentCandidate = () => {
-    }
-
-    // useEffect(() => {
-    //     if (toolTipVisibility) {
-    //         setCurrentCandidate('testing')
-    //     }
-    // }, [ toolTipVisibility ]) // TODO
 
     return (
         <div>
@@ -105,27 +104,22 @@ const App = () => {
                     node={node}
                     key={i}
                     candidate={c}
-                    handleHover={c => toggleToolTipVisibility(c.name)}
-                    handleClick={() => setCurrentCandidate(c.name)}
+                    handleHover={() => handleHover(c.name)}
+                    handleClick={() => setClickedCandidate(c.name)}
                     />
             )) }
             <p>image credit: Politico</p>
-            { toolTipVisibility? (
-                <CandidatePreview>
-                    Click to see stats on {hoverTarget}
-                </CandidatePreview>
-            ) : (
-                <CandidatePreview>
-                    Click on a candidate to see their stats.
-                </CandidatePreview>
-            )}
-            { !!currentCandidate && (
-                <ToolTip>
-                    Some facts about {currentCandidate}:
-                    * loves cheese
-                    * has a Twitter account
-                </ToolTip>
-            )}
+
+            { hoverCandidate
+                ? <CandidateStats candidate={hoverCandidate} />
+                : clickedCandidate
+                    ? <CandidateStats candidate={clickedCandidate} />
+                    : (
+                        <ToolTip>
+                            Click on a candidate to see their stats.
+                        </ToolTip>
+                    )
+            }
         </div>
     )
 }
