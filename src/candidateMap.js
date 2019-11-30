851eb44cb2810e0a7aaa84861d7866cb6c70aeb0
Kairsten Fay <kairsten.fay@gmail.com>
2019-11-29 22:25:37 -0800

    Refactor candidateMap into an import

---
+++ src/candidateMap.js
@@ -0,0 +1,50 @@
+import Warren from "./img/ewarren.png"
+import Sanders from "./img/BernieSanders.png"
+import Biden from "./img/JoeBiden.png"
+import Yang from "./img/AndrewYang.png"
+import Buttigieg from "./img/PeteButtigieg.png"
+import Booker from "./img/CoryBooker.png"
+import Gabbard from "./img/TulsiGabbard.png"
+import Harris from "./img/KamalaHarris.png"
+import Klobuchar from "./img/AmyKlobuchar.png"
+// import Steyer from "./img/TomSteyer.png"
+
+
+export default {
+    'ewarren': {
+    name: 'Elizabeth Warren',
+    photo: Warren,
+    },
+    'BernieSanders': {
+        name: 'Bernie Sanders',
+        photo: Sanders,
+    },
+    'JoeBiden': {
+        name: 'Joe Biden',
+        photo: Biden,
+    },
+    'AndrewYang': {
+        name: 'Andrew Yang',
+        photo: Yang,
+    },
+    'PeteButtigieg': {
+        name: 'Pete Buttigieg',
+        photo: Buttigieg,
+    },
+    'TulsiGabbard': {
+        name: 'Tulsi Gabbard',
+        photo: Gabbard,
+    },
+    'KamalaHarris': {
+        name: 'Kamala Harris',
+        photo: Harris,
+    },
+    'amyklobuchar': {
+        name: 'Amy Klobuchar',
+        photo: Klobuchar,
+    },
+    'CoryBooker': {
+        name: 'Cory Booker',
+        photo: Booker,
+    },
+}
--- src/index.js
+++ src/index.js
@@ -3,58 +3,10 @@ import ReactDOM from "react-dom"
 import styled from "styled-components"
 import './main.css';
 
-import Warren from "./img/ewarren.png"
-import Sanders from "./img/BernieSanders.png"
-import Biden from "./img/JoeBiden.png"
-import Yang from "./img/AndrewYang.png"
-import Buttigieg from "./img/PeteButtigieg.png"
-import Booker from "./img/CoryBooker.png"
-import Gabbard from "./img/TulsiGabbard.png"
-import Harris from "./img/KamalaHarris.png"
-import Klobuchar from "./img/AmyKlobuchar.png"
+import candidateMap from "./candidateMap.js"
 
 const data = require("./data/emojis.json")
-// import Steyer from "./img/TomSteyer.png"
 
-
-const candidateMap = {
-    'ewarren': {
-		name: 'Elizabeth Warren',
-        photo: Warren,
-	},
-    'BernieSanders': {
-		name: 'Bernie Sanders',
-        photo: Sanders,
-	},
-    'JoeBiden': {
-		name: 'Joe Biden',
-        photo: Biden,
-	},
-    'AndrewYang': {
-		name: 'Andrew Yang',
-        photo: Yang,
-	},
-    'PeteButtigieg': {
-		name: 'Pete Buttigieg',
-        photo: Buttigieg,
-	},
-    'TulsiGabbard': {
-		name: 'Tulsi Gabbard',
-        photo: Gabbard,
-	},
-    'KamalaHarris': {
-		name: 'Kamala Harris',
-        photo: Harris,
-	},
-    'amyklobuchar': {
-		name: 'Amy Klobuchar',
-        photo: Klobuchar,
-	},
-    'CoryBooker': {
-		name: 'Cory Booker',
-        photo: Booker,
-	},
-}
 
 const Img = styled.img`
   border-radius: 50%;
