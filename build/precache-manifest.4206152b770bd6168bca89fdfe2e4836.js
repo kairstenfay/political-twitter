dca898297c8d67498b9e8bf42e73e5406fd8f5cc
Kairsten Fay <kairsten.fay@gmail.com>
2019-12-01 00:26:28 -0800

    Add build directory

---
+++ build/precache-manifest.4206152b770bd6168bca89fdfe2e4836.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d5bd8e59369e261037afd2591177f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8934e62e19f74c32b2e5",
+    "url": "/static/css/main.1517dde8.chunk.css"
+  },
+  {
+    "revision": "fb09cb81b277511ea19f",
+    "url": "/static/js/2.93b9edaa.chunk.js"
+  },
+  {
+    "revision": "8934e62e19f74c32b2e5",
+    "url": "/static/js/main.4fa24cab.chunk.js"
+  },
+  {
+    "revision": "7212ba25e3533868ee09",
+    "url": "/static/js/runtime-main.6a3c3cbd.js"
+  },
+  {
+    "revision": "2f49e4aae9225999a2f0fdefc8e306a6",
+    "url": "/static/media/AmyKlobuchar.2f49e4aa.png"
+  },
+  {
+    "revision": "8e58361096e31a952ce4304b0bc8c46e",
+    "url": "/static/media/AndrewYang.8e583610.png"
+  },
+  {
+    "revision": "fca28f5baa14ab10f7cd07d8e61b2150",
+    "url": "/static/media/BernieSanders.fca28f5b.png"
+  },
+  {
+    "revision": "a5be3b6b7ed88b3824d0228d2c199e7c",
+    "url": "/static/media/CoryBooker.a5be3b6b.png"
+  },
+  {
+    "revision": "a942a0190cce394211cf8b682a2c316a",
+    "url": "/static/media/JoeBiden.a942a019.png"
+  },
+  {
+    "revision": "cc2f9357860628cb2a2e2f0c88af5bf1",
+    "url": "/static/media/KamalaHarris.cc2f9357.png"
+  },
+  {
+    "revision": "0c361d794a2364adabb53cf211f285cc",
+    "url": "/static/media/MikeBloomberg.0c361d79.png"
+  },
+  {
+    "revision": "ffc9f511292f24a6426edca21dae6804",
+    "url": "/static/media/PeteButtigieg.ffc9f511.png"
+  },
+  {
+    "revision": "41c0e2885c388fd45c41afd894e02667",
+    "url": "/static/media/TomSteyer.41c0e288.png"
+  },
+  {
+    "revision": "dd3da2a9b9ebd356f789043ad580046a",
+    "url": "/static/media/TulsiGabbard.dd3da2a9.png"
+  },
+  {
+    "revision": "a4fb84ae989f1d92ed3c18afbac39075",
+    "url": "/static/media/ewarren.a4fb84ae.png"
+  },
+  {
+    "revision": "2c64644f9a906fad8f7a5473120702ed",
+    "url": "/static/media/twitter.2c64644f.png"
+  }
+]);
